1bbac6f2437da074d101293a6f33ab79d81e8518
Zeyuan Zhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-05-19 03:49:57 +0000

    theme toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+
 export default function Page() {
   return (
     <div>
       <h1 className="text-sm">Hello World</h1>
+      <ThemeToggle />
     </div>
   );
 }
