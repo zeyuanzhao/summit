50bd2c80e512b7266039ff9a8551d53849dd0f1f
Zeyuan Zhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-05-19 03:11:44 +0000

    protect auth pages

---
+++ app/auth/(protected)/layout.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { createClient } from "@/lib/supabase/client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const router = useRouter();
+  useEffect(() => {
+    const supabase = createClient();
+    supabase.auth.getUser().then((user) => {
+      if (!user.data || !user.data.user) {
+        router.push("/auth/login");
+      }
+    });
+  }, [router]);
+
+  return children;
+}
