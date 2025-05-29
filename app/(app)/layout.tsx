09886ed7fba02671eb55e4fc33945a95e46b326c
Zeyuan Zhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-05-29 03:55:50 +0000

    supabase fetch auth layout

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,14 +1,39 @@
+"use client";
+
+import { User } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
+import type { ReactNode } from "react";
+import { useEffect, useState } from "react";
+
+import { UserContext } from "@/components/context/user-context";
 import { Sidebar } from "@/components/sidebar";
+import { createClient } from "@/lib/supabase/client";
 
 export default function Layout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
+  const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const supabase = createClient();
+    supabase.auth.getUser().then(({ data }) => {
+      if (!data?.user) {
+        router.push("/auth/login");
+      } else {
+        setUser(data.user);
+      }
+    });
+  }, [router]);
+
   return (
-    <div className="flex h-screen w-screen flex-row">
-      <Sidebar />
-      {children}
-    </div>
+    <UserContext.Provider value={user}>
+      <div className="flex h-screen w-screen flex-row">
+        <Sidebar />
+        {children}
+      </div>
+    </UserContext.Provider>
   );
 }
---
+++ components/context/user-context.tsx
@@ -0,0 +1,4 @@
+import type { User } from "@supabase/supabase-js";
+import { createContext } from "react";
+
+export const UserContext = createContext<User | null>(null);
