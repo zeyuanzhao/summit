e118735101137fb1e0a4b7ba1b43ebd0bd2b4934
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-06-28 17:26:17 -0400

    fetch list papers

---
+++ summit-next/src/app/(app)/list/[id]/ListPage.tsx
@@ -0,0 +1,11 @@
+import { List, Paper } from "@/interfaces";
+
+export function ListPage({
+  list,
+  listPapers = [],
+}: {
+  list: List[];
+  listPapers?: Paper[];
+}) {
+  return <div />;
+}
--- summit-next/src/app/(app)/list/[id]/page.tsx
+++ summit-next/src/app/(app)/list/[id]/page.tsx
@@ -1,3 +1,78 @@
-export default function Page() {
-  return <div />;
+import camelcaseKeys from "camelcase-keys";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+import { ErrorMessage } from "@/components/error";
+import { createClient } from "@/lib/supabase/server";
+import { listSchema } from "@/lib/validation/list";
+import { paperSchema } from "@/lib/validation/paper";
+
+import { ListPage } from "./ListPage";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
+  if (!id) {
+    return (
+      <ErrorMessage
+        title="List Not Found"
+        error="The requested list does not exist."
+      />
+    );
+  }
+  const supabase = await createClient();
+  const { data: user, error: userError } = await supabase.auth.getUser();
+
+  if (!user || userError) {
+    return redirect("/auth/login");
+  }
+
+  const listsPromise = supabase
+    .from("list")
+    .select("*")
+    .eq("user_id", user.user.id);
+
+  const listPapersPromise = supabase
+    .from("list_paper")
+    .select("papers(*)")
+    .eq("list_id", id);
+
+  const [listsResult, listPapersResult] = await Promise.all([
+    listsPromise,
+    listPapersPromise,
+  ]);
+
+  const { data: lists, error: errorLists } = listsResult;
+  const { data: listPapers, error: errorListPapers } = listPapersResult;
+
+  if (errorLists || errorListPapers) {
+    return <ErrorMessage title="Error" error="Failed to fetch data." />;
+  }
+
+  const zodLists = z.array(listSchema).safeParse(camelcaseKeys(lists));
+  if (!zodLists.success) {
+    return <ErrorMessage title="Error" error="Invalid list data." />;
+  }
+
+  const zodListPapers = z
+    .array(paperSchema)
+    .safeParse(camelcaseKeys(listPapers));
+  if (!zodListPapers.success) {
+    return <ErrorMessage title="Error" error="Invalid papers data." />;
+  }
+
+  const parsedLists = zodLists.data;
+  const parsedListPapers = zodListPapers.data;
+  if (parsedLists.length === 0 || !parsedLists.some((list) => list.id === id)) {
+    return (
+      <ErrorMessage
+        title="List Not Found"
+        error="The requested list does not exist."
+      />
+    );
+  }
+  return (
+    <div>
+      <ListPage list={parsedLists} listPapers={parsedListPapers} />
+    </div>
+  );
 }
